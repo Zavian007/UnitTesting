a6b3ddd480193eb5a27275afa24983900b199096
ali mohamed <alimohamedreddit@gmail.com>
2021-03-29 19:36:49 +0200

    First commit

---
+++ src/test/java/za/ac/cput/testunits/ProjectTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.testunits;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student name - 219113505
+ */
+public class ProjectTest {
+    
+}
