3ae665228c77227377cb3fa68b4772ab7439d8bd
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2021-04-01 18:14:30 +0200

    Update Project.java

--- src/main/java/za/ac/cput/testunits/Project.java
+++ src/main/java/za/ac/cput/testunits/Project.java
@@ -1,14 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package za.ac.cput.testunits;
+import java.util.Objects;
 
 /**
- *
- * @author Student name - 219113505
+ * Applications Development Practice (ADP262) Assignment1 
+ * Zavian Minnies
+ * 217230458
  */
+
 public class Project {
+    
+    Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.subject, other.subject)) {
+            return false;
+        }
+        return true;
+    }
+
+    private String name, id, subject;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", id=" + id + ", subject=" + subject + '}';
+    }
+     
+    
+    
+}
+
+
     
 }
