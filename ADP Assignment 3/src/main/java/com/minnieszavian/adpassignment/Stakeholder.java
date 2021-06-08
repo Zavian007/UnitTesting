c92ca0dd9c766d0531f25e7a8149ab733ccfa81c
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2021-06-09 00:03:14 +0200

    Add files via upload

---
+++ ADP Assignment 3/src/main/java/com/minnieszavian/adpassignment/Stakeholder.java
@@ -0,0 +1,33 @@
+package com.minnieszavian.adpassignment;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Zavian Minnies: 217230458
+ */
+
+public class Stakeholder implements Serializable{
+    private String stHolderId;
+
+    public Stakeholder() {
+    }
+    
+    public Stakeholder(String stHolderId) {
+        this.stHolderId = stHolderId;
+    }
+    
+    public String getStHolderId() {
+        return stHolderId;
+    }
+
+    public void setStHolderId(String stHolderId) {
+        this.stHolderId = stHolderId;
+    }
+
+    @Override
+    public String toString() {
+       return stHolderId;
+    }
+
+}
